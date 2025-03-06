600012e1f49c7979774ae8ed6915e1cdd6063512
unknown <wael.mrabet.dev@gmail.com>
2025-03-06 03:10:42 +0100

    first commit initialize web app ng 17

---
+++ src/app/bungalows/bungalows.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BungalowsRoutingModule } from './bungalows-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BungalowsRoutingModule
+  ]
+})
+export class BungalowsModule { }
