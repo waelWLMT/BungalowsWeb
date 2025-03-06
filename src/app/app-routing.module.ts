4cf9165f63eba27f2a11d1b0d15a84d4d25b241d
unknown <wael.mrabet.dev@gmail.com>
2025-03-06 22:37:04 +0100

    add liste of modules as features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bungalows',
+    loadChildren: () => import('./features/bungalows/bungalows.module').then(m => m.BungalowsModule)
+  },
+  {
+    path: 'clients',
+    loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'reservations',
+    loadChildren: () => import('./features/reservations/reservations.module').then(m => m.ReservationsModule)
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path:'**',
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
